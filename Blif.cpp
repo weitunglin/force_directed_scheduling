5a7f39c3ecce1641127cf465c3a43eb453397f85
weitunglin <weitung8@gmail.com>
2021-10-31 01:03:04 +0800

    initial commit

---
+++ Blif.cpp
@@ -0,0 +1,1 @@
+#include "Blif.h"
---
+++ Blif.h
@@ -0,0 +1,22 @@
+#ifndef BLIF_H
+#define BLIF_H
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+
+/*
+FDS algorithm
+repeat {
+  run ALAP, ASAP
+  compute the opeartion and type prob.
+  compute the self-force, ps-force and total force
+  scheulde the op. with least force
+}
+until (all opeartions are schedules)
+*/
+
+class Blif {
+};
+
+#endif
